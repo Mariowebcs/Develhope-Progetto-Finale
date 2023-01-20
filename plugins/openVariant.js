1786240da12b6d24c859c357f2fe69f0bb329903
Mario D'Andrea <mariodandrea2595@gmail.com>
2023-01-20 02:52:32 +0100

    add: added all files and committing for the first time, ready to host on github

---
+++ plugins/openVariant.js
@@ -0,0 +1,10 @@
+
+
+const plugin = require("tailwindcss/plugin");
+
+const openVariant = plugin(function({addVariant}){
+    addVariant("group-open", ":merge(.group).open &");
+    addVariant("peer-open", ":merge(.peer).open ~ &");
+})
+
+module.exports = openVariant;
