74cf2ab494c782ef09bb3816e9d82722582530ef
Mario D'Andrea <mariodandrea2595@gmail.com>
2023-01-24 03:13:14 +0100

    feat : added dark mode toggle with all styles and added cards design with icon for count  peoples

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode : 'class',
   content: ["./src/**/*.{html,js}"],
   theme: {},
   plugins: [require("./plugins/openVariant")],
