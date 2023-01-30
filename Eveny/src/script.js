af48fb8dab57ff53be9b77f633bf1b61cf1990c9
Mario D'Andrea <mariodandrea2595@gmail.com>
2023-01-30 02:46:35 +0100

    fix : apply method to fix video background on mac, ready to check, update card design

--- Eveny/src/script.js
+++ Eveny/src/script.js
@@ -1,10 +1,5 @@
-
-
-
-
-
+//Observer script
 let elementsToWatch = document.querySelectorAll('.watch');
-
 let callback = function (items) {
     items.forEach((item) => {
         if (item.isIntersecting) {
@@ -14,10 +9,20 @@ let callback = function (items) {
         }
     })
 }
-
-
-let observer = new IntersectionObserver(callback, { threshold: 0.6 });
-
+let observer = new IntersectionObserver(callback, { threshold: 0.5 });
 elementsToWatch.forEach((element) => {
     observer.observe(element);
-})
+});
+
+//Scroll check for navbar
+let previousScroll;
+let navbar = document.getElementById("nav");
+window.addEventListener("scroll", function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > previousScroll) {
+        navbar.style.top = "-100px";
+    } else {
+        navbar.style.top = "0";
+    }
+    previousScroll = scrollTop;
+});
