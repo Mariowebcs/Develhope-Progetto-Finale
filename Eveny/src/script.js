8acb44d95cfa5a0b633fa970a67a1f73aa34b33b
Mario D'Andrea <mariodandrea2595@gmail.com>
2023-02-04 01:48:56 +0100

    fix : fixed autotype text and padding in mobile and desktop version, ready to check

--- Eveny/src/script.js
+++ Eveny/src/script.js
@@ -15,30 +15,34 @@ elementsToWatch.forEach((element) => {
     observer.observe(element);
 });
 
+//SCRIPT CREATE CARD EXAMPLE FROM INPUT
+const eventTitle = document.getElementById("input-title");
+const eventDescription = document.getElementById("input-description");
+const cardCreate = document.getElementById("card-created");
+const title = document.getElementById("event__Title");
+const description = document.getElementById("event__Description");
+const button = document.getElementById("create-event");
 
-          const eventTitle = document.getElementById("input-title");
-          const eventDescription = document.getElementById("input-description");
-          const cardCreate = document.getElementById("card-created");
-          const title = document.getElementById("event__Title");
-          const description = document.getElementById("event__Description");
+if (title.innerHTML === "" && description.innerHTML === "") {
+    cardCreate.classList.add("opacity-0");
+} else {
+    cardCreate.classList.add("opacity-100");
+}
 
-          const button = document.getElementById("create-event");
-
-          button.onclick = async function createEvent() {
-            if (title.innerHTML === "" && description.innerHTML === "") {
-              title.innerText = eventTitle.value;
-              description.innerText = eventDescription.value;
-              if(title.innerHTML!=="" && description.innerHTML!==""){
-                cardCreate.classList.remove("opacity-0");
-              cardCreate.classList.add("opacity-100");
-            }              
-            } else {
-              alert("L'evento è gia stato creato!");
-            }
-          };
-
-          if (title.innerHTML === "" && description.innerHTML === "") {
-            cardCreate.classList.add("opacity-0");
-          } else {
+button.onclick = async function createEvent() {
+    if (title.innerHTML === "" && description.innerHTML === "") {
+        if (eventTitle.value !== "" && eventDescription.value !== "") {
+            title.innerText = eventTitle.value;
+            description.innerText = eventDescription.value;
+        } else {
+            alert("Attenzione compilare entrambi i campi")
+        }
+        if (title.innerHTML !== "" && description.innerHTML !== "") {
+            cardCreate.classList.remove("opacity-0");
             cardCreate.classList.add("opacity-100");
-          }
+        }
+    } else {
+        alert("L'evento è gia stato creato!");
+    }
+};
+
