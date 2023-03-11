47d3b8478504e8786daa66601ec108a5b5554089
Silvio.Craparotta <craparottasilvio@gmail.com>
2023-03-11 23:53:30 +0100

    Modificata Navbar

--- eveny/src/components/shared/MobileMenu.js
+++ eveny/src/components/shared/MobileMenu.js
@@ -1,10 +1,16 @@
 import React from "react";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBell, faBookmark, faCalendarDays, faCirclePlus, faHouse } from '@fortawesome/free-solid-svg-icons'
 
 export function MobileMenu() {
     return(
         <div className="containerMenu flex justify-center h-16">
-            <div className="menu flex justify-between w-64 h-12 BGcolor rounded-full ">
+            <div className="menu flex justify-around w-56 h-12 BGcolor rounded-full ">
+                <button><FontAwesomeIcon icon={faHouse} inverse/></button>
+                <button><FontAwesomeIcon icon={faCalendarDays} inverse/></button>
+                <button className="text-3xl"><FontAwesomeIcon icon={faCirclePlus} inverse/></button>
+                <button><FontAwesomeIcon icon={faBookmark} inverse/></button>
+                <button><FontAwesomeIcon icon={faBell} inverse/></button>
             </div>
         </div>
     )
--- eveny/src/components/shared/Navbar.js
+++ eveny/src/components/shared/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import avatar1 from "./assets/avatar1.jpg"
 import evenylogo from "./assets/evenylogo.png"
 import searchicon from "./assets/searchicon.png"
-import { useState } from "react";
+import { useState } from "react"
 
 export function Navbar() {
 
@@ -14,48 +14,31 @@ export function Navbar() {
     setShowMenu(!showMenu);
   };
 
-  // handle placeholder
-
-  const [placeholder, setPlaceholder] = useState("    Cerca evento");
-
-  const handleFocusPlaceholder = () => {
-    setPlaceholder('');
-  };
-
-  const handleBlurPlaceholder = () => {
-    setPlaceholder("    Cerca evento");
-  };
-
-  // handle search icon
-
-  const [showSearchIcon, setShowSearchIcon] = useState(true)
-
-  const handleFocusSearchIcon = () => {
-    setShowSearchIcon(false)
-  }
-
-  const handleBlurSearchIcon = () => {
-    setShowSearchIcon(true)
-  }
-  
-  
 
   return (
 
     <nav className="NavContainer p-4 h-48 BGcolor w-full">
       
-      {/* Container Logo + User */}      
-      <div className="NavTop mx-auto  mb-4 flex justify-between">
-        <div className="NavLogo w-12 h-12 rounded-full overflow-hidden">
-          <img className="w-full h-full object-cover noDrag noSelect" src={evenylogo} alt="LogoEveny" />
+      {/* Container Logo + User */} 
+      <div className="NavTop mx-auto  mb-2 flex justify-between">
+
+        {/* Logo */}
+        <div className="flex flex-col ">
+          <div className="NavLogo w-12 h-12 rounded-full overflow-hidden">
+            <img className="w-full h-full object-cover noDrag noSelect" src={evenylogo} alt="LogoEveny" />
+          </div>
+          <h4 className="text-center font-bold tracking-tight text-transparent bg-clip-text bg-gradient-to-b from-[#6C21A7] to-[#DB2777]">Eveny</h4>
         </div>
+
+        {/* Avatar */}
         
-        <div className="NavAvatar w-12 h-12 rounded-full overflow-hidden noSelect"  onClick={handleAvatarClick}>
+        <div className="NavAvatar w-12 h-12 rounded-full overflow-hidden noSelect"  onClick={handleAvatarClick} onBlur={handleAvatarClick}>
           <img className="w-full h-full object-cover noDrag noSelect" src={avatar1} alt="UserAvatar" />
         </div>
 
+
         {showMenu && (
-          <div className="NavMenu noSelect absolute top-[70px] right-2 rounded-lg h-20 w-20 bg-neutral-100 bg-opacity-80 shadow-lg">
+          <div className="NavMenu noSelect absolute z-50 top-[70px] right-2 rounded-lg h-20 w-20 bg-neutral-100 bg-opacity-80 shadow-lg">
             <ul className="flex flex-col h-20 justify-around text-center text-xs text-slate-600">
               <li className="hover:bg-neutral-200 bg-opacity-80 rounded-lg py-1"><a href="https://www.w3schools.com">Preferenze</a></li>
               <li className="hover:bg-neutral-200 bg-opacity-80 rounded-lg py-1"><a href="https://www.w3schools.com">Profilo</a></li>
@@ -75,17 +58,17 @@ export function Navbar() {
       </div>
 
       {/* SearchBar Text Input */}
-      <div className="NavSearch mx-auto flex justify-center items-center">
-        <div className="NavSearch">
-          {showSearchIcon && <img className="searchicon noDrag" src={searchicon} alt=""/>}
-          <input 
-          className="w-64 h-9 rounded-full py-2 px-4 text-white outline-none focus:outline-pink-600 focus:outline-2 placeholder-gray-500 SearchBG" 
-          type="text" 
-          placeholder={placeholder}
-          onBlur={() => {handleBlurPlaceholder(); handleBlurSearchIcon()}} 
-          onFocus={() => {handleFocusPlaceholder(); handleFocusSearchIcon()}}
-          />
+      <div className="relative flex justify-center">
+        
+        <div className="absolute inset-y-0 mr-52 flex items-center pointer-events-none">
+          <img className="searchicon noDrag" src={searchicon} alt=""/>
         </div>
+
+        <input
+          type="text"
+          className="searchInput w-64 h-9 rounded-full  pl-10 py-2 px-4 text-white outline-none focus:outline-[#9333EA] focus:outline-2 placeholder-gray-500 placeholder-opacity-75 SearchBG"
+          placeholder="Cerca Evento"
+        />
       </div>
 
     </nav>
