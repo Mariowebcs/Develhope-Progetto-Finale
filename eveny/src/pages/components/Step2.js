bfb26fbfb5138b936c13afd4e1ed87690dd98322
Manuel Luppino <60667104+Warlup@users.noreply.github.com>
2023-03-24 00:43:29 +0100

    update step-2 with more logic and stability

--- eveny/src/pages/components/Step2.js
+++ eveny/src/pages/components/Step2.js
@@ -5,26 +5,47 @@ import itemsArray from "../../components/json/items.json";
 const Step2 = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedItems, setSelectedItems] = useState([]);
+  const [buttonVisible, setButtonVisible] = useState(Array(itemsArray.length).fill(true)); // tramite il fill tramutiamo tutti i valori dell'array in true
   const navigate = useNavigate();
 
   const handleInputChange = (event) => {
     //testo search bar
     setSearchTerm(event.target.value);
   };
-
+  // filtra testo di ricerca sia per lowerCase che per Uppercase
   const filteredArray = itemsArray.filter((item) => {
-    return item.interests.includes(searchTerm);
+    const interests = item.interests.toLowerCase();
+    const searchTermLower = searchTerm.toLowerCase();
+    const searchTermUpper = searchTerm.toUpperCase();
+    return interests.includes(searchTermLower) || interests.includes(searchTermUpper);
   })
+  // filtra l'array dello useState selectedItems
+  const uniqueItems = selectedItems.filter((item, index) => {
+    return selectedItems.indexOf(item) === index;
+  });
 
-  const handleRemoveItems = (index) => {
-    //rimuove items
-    setSelectedItems(selectedItems.filter((_, i) => i !== index));
-  };
-
-  const onSearch = (search) => {
+  // Al click del bottone tag preferito viene aggiunto all'array e viene nascosto tramite setButtonVisible
+  const onSearch = (search, index) => {
     setSearchTerm(search);
     setSelectedItems([...selectedItems, search]);
-    setSearchTerm('')
+    setSearchTerm('');
+    // rende invisibile il bottone appena cliccato
+    const newButtonVisibility = [...buttonVisible];
+    newButtonVisibility[index] = false;
+    setButtonVisible(newButtonVisibility);
+  };
+
+  //rimuove gli items dall'array di selectedItems
+  const handleRemoveItems = (index) => {
+    const updateItems = [...selectedItems];
+    if (updateItems.includes(selectedItems[index])) {
+      updateItems.splice(index, 1);
+      setSelectedItems(updateItems);
+    }
+    // riabilita il bottone corrispondente nella sezione dei bottoni selezionabili
+    const newButtonVisibility = [...buttonVisible];
+    newButtonVisibility[itemsArray.findIndex((item) => item.interests === selectedItems[index])] = true;
+    setButtonVisible(newButtonVisibility);
   };
 
   const registerData = JSON.parse(localStorage.getItem("registerData"));
@@ -45,38 +66,30 @@ const Step2 = () => {
       >
         <div className="absolute top-10 flex flex-col gap-4 w-max-72 items-center justify-center">
           <h1 className="text-2xl font-bold mb-4">Choose Your Interests</h1>
-          <form>
+          <div>
             <input
               onChange={handleInputChange}
               className="border-2 border-purple-600 w-full rounded-2xl p-1 focus:outline-none" />
-          </form>
-          <div className="h-[255px] overflow-y-scroll scrollbar">
-            {filteredArray.length > 0 && filteredArray.map((item, index) => (
-              <button
-                key={index}
-                className="cursor-pointer text-start px-2 mt-1 mb-1 mx-3 border-purple-800 border-solid border-2 rounded-full"
-                onClick={() => onSearch(item.interests)}
-              >
-                {item.interests}
-              </button>
-            ))}
-            {!filteredArray.length > 0 && itemsArray.map((item, index) => (
-              <button
-                key={index}
-                className="cursor-pointer text-start px-2 mt-1 mb-1 mx-3 border-purple-800 border-solid border-2 rounded-full"
-                onClick={() => onSearch(item.interests)}
-              >
-                {item.interests}
-              </button>
-            )) }
           </div>
+          <div className="h-[180px] overflow-y-scroll scrollbar w-[95%]">
+            {filteredArray.map((item, index) => (
+              buttonVisible[index] && (
+                <button
+                  key={index}
+                  className="cursor-pointer text-start px-2 mt-1 mb-1 mx-3 border-purple-800 border-solid border-2 rounded-full"
+                  onClick={() => onSearch(item.interests, index)}
+                >
+                  {item.interests}
+                </button>
 
-
+              )
+            ))}
+          </div>
           {/* i miei interessi */}
           <div className="flex flex-col gap-4 items-center justify-center">
             <h2 className="text-xl">My Interests</h2>
             <ul className="flex gap-2 flex-wrap pl-3 pr-3">
-              {selectedItems.map((item, index) => (
+              {uniqueItems.map((item, index) => (
                 <li
                   key={index}
                   className="bg-purple-800 rounded-3xl p-1 text-white w-fit"
