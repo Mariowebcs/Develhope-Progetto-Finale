1461b42702fcbe88c84e283347b65994234795c1
Andrea <andrea.ciambriello@hotmail.com>
2023-03-27 22:25:25 +0200

    Delete old login folder

--- login/src/components/SubmitButton.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const SubmitButton = ( { label, buttonStyle, onClick, isDisabled } ) => {
-    return (
-        <button type="submit" onClick={onClick} className={buttonStyle} disabled={isDisabled}>{label}</button>
-    )
-}
